9ce03e97c620ee4c54555545c51a3fbc473cf068
NullTerrier <marek.aureliusz.makuch@gmail.com>
2018-10-19 21:04:15 +0200

    Added Entities, basci relations

---
+++ src/main/java/guru/springframework/domain/Notes.java
@@ -0,0 +1,20 @@
+package guru.springframework.domain;
+/*
+ * @author Marecki
+ */
+
+import javax.persistence.*;
+
+@Entity
+public class Notes {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @OneToOne
+    private Recipe recipe;
+
+    @Lob
+    private String recipeNotes;
+}
